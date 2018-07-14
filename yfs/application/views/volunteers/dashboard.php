1d05c2ef2b3d11d2b1aaab29fa0b6ce75a92ab9e
Siddharth Panigrahi <siddharth7997@gmail.com>
2018-07-14 18:06:49 +0530

    updated side nav

--- yfs/application/views/volunteers/dashboard.php
+++ yfs/application/views/volunteers/dashboard.php
@@ -1,85 +1,72 @@
-<div id="wrapper">
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-		<div class="container-fluid">
-			<div class="navbar-header">
-    			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        			<span class="sr-only">Toggle navigation</span>
-        			<span class="icon-bar"></span>
-        			<span class="icon-bar"></span>
-        			<span class="icon-bar"></span>
-    			</button>
-                <div  class="navbar-brand">
-                    <a id="menu-toggle" href="#" class="glyphicon glyphicon-align-justify btn-menu toggle">
-                        <i class="fa fa-bars"></i>
-                    </a>
-    				<a href="#">Project name</a>
-                </div>
-			</div>
-			<div id="navbar" class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li class="active"><a href="#">Home</a></li>
-					<li><a href="#about">About</a></li>
-				</ul>
-			</div><!--/.nav-collapse -->
-		</div>
-	</nav>
-    <!-- Sidebar -->
-    <div id="sidebar-wrapper">
-        <nav id="spy">
-            <ul class="sidebar-nav nav">
-                <li class="sidebar-brand">
-                    <a href="#home"><span class="fa fa-home solo">Home</span></a>
-                </li>
-                <li>
-                    <a href="#anch1">
-                        <span class="fa fa-anchor solo">Anchor 1</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#anch2">
-                        <span class="fa fa-anchor solo">Anchor 2</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#anch3">
-                        <span class="fa fa-anchor solo">Anchor 3</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#anch4">
-                        <span class="fa fa-anchor solo">Anchor 4</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    <!-- Page content -->
-    <div id="page-content-wrapper">
-        <div class="page-content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="panel panel-danger">
-                            <div class="panel-heading">
-                                    Panel 1
-                            </div>
-                            <div class="panel-body">
-                                content body
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">                    
-                        <div class="panel panel-success">
-                            <div class="panel-heading">
-                                    Panel 1
-                            </div>
-                            <div class="panel-body">
-                                content body
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<style>
+        body {
+            font-family: "Lato", sans-serif;
+        }
+
+        .sidenav {
+            height: 100%;
+            width: 0;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 0;
+            background-color: #111;
+            overflow-x: hidden;
+            transition: 0.5s;
+            padding-top: 60px;
+        }
+
+        .sidenav a {
+            padding: 8px 8px 8px 32px;
+            text-decoration: none;
+            font-size: 25px;
+            color: #818181;
+            display: block;
+            transition: 0.3s;
+        }
+
+        .sidenav a:hover {
+            color: #f1f1f1;
+        }
+
+        .sidenav .closebtn {
+            position: absolute;
+            top: 0;
+            right: 25px;
+            font-size: 36px;
+            margin-left: 50px;
+        }
+
+        @media screen and (max-height: 450px) {
+        .sidenav {padding-top: 15px;}
+        .sidenav a {font-size: 18px;}
+        }
+</style>
+
+<script>
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+        }
+
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+        }
+</script>
+
+
+</head>
+<body>
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="http://localhost/createevent.php">Create Event</a>
+  <a href="#">View Event</a>
+  
 </div>
+
+<h2 align="center">Admin</h2>
+
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+
+     
+</body>
