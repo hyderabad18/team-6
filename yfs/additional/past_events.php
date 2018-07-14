f5377399c0b0d174dfc842b713bd83a4a2652e31
amulyametku <devi.amulya72@gmail.com>
2018-07-14 21:42:18 +0530

    volunteer past events

---
+++ yfs/additional/past_events.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  
+  <div class="list-group">
+    <a href="#" class="list-group-item list-group-item-action">Action item</a>
+    <a href="#" class="list-group-item list-group-item-action list-group-item-success">Success item</a>
+    <a href="#" class="list-group-item list-group-item-action list-group-item-secondary">Secondary item</a>
+    <a href="#" class="list-group-item list-group-item-action list-group-item-info">Info item</a>
+  
+    <a href="#" class="list-group-item list-group-item-action list-group-item-light">Light item</a>
+  </div>
+</div>
+
+</body>
+</html>
