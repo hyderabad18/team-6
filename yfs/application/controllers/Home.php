26d83bb770d836a37f46f85a1f8da49c00844733
Manish-Kumar-Sadhu <sadhumanishkumar@gmail.com>
2018-07-14 21:31:06 +0530

    modified authentication process

--- yfs/application/controllers/Home.php
+++ yfs/application/controllers/Home.php
@@ -11,7 +11,8 @@ class Home extends CI_Controller
 		parent::__construct();
 		$this->load->helper('form');
 		$this->load->library('form_validation');
-
+		$this->load->model("volunteer_model");
+		
 		$this->config->load('site_settings', TRUE);
 
 		$this->site_config = $this->config->item('site_settings');
@@ -23,12 +24,16 @@ class Home extends CI_Controller
 		//header("Expires: Fri, 4 Jun 2010 12:00:00 GMT");
 	}// __construct
 
-
+        
+        public function login(){
+            $this->load->view('users/login');
+        }// login
+        
         public function index() {
             $this->login_user();
             $this->page_data['title'] = "Home|" . $this->site_name;
             
-            $this->load->view('home/home', $this->page_data);
+            $this->load->view('users/login');
         }
     
 
@@ -43,33 +48,26 @@ class Home extends CI_Controller
                 $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
                 $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[8]|callback_login_credentials_check');
                 if ($this->form_validation->run()) {
-                    $user_data = $this->user->get_user_data($this->input->post('email'));
+                    $user_data = $this->volunteer_model->get_user_data($this->input->post('email'));
                     $session_data = array(
-                        'user_name' => $user_data['DisplayName'],
-                        'user_id' => $user_data['UserID'],
-                        'DOB' => $user_data['DOB'],
+                        'volunteer_name' => $user_data['DisplayName'],
+                        'volunteer_id' => $user_data['volunteer_id'],
                         'Gender' => $user_data['Gender'],
-                        'phone' => $user_data['Phone'],
-                        'altPhone' => $user_data['AlternatePhone'],
+                        'phone' => $user_data['phone_no'],
                         'firstName' => $user_data['FirstName'],
                         'lastName' => $user_data['LastName'],
-                        'blood_group' => $user_data['BloodgroupID'],
+                        'displayName' => $user_data['display_name'],
                         'email' => $user_data['EmailID'],
-                        'Role' => $user_data['Role'],
-                        'CordApproveStatus' => $user_data['CordApproveStatus'],
-                        'BaseLat' => $user_data['BaseLat'],
-                        'BaseLong' => $user_data['BaseLong'],
-                        'FreqAvailLat' => $user_data['FreqAvailLat'],
-                        'FreqAvailLong' => $user_data['FreqAvailLong'],
-                        'LastBloodDonatedOn' => $user_data['LastBloodDonatedOn'],
-                        'AvailabilityStatusID' => $user_data['AvailabilityStatusID'],
-                        'AvailabilityDate' => $user_data['AvailabilityDate'],
+                        'location_name' => $user_data['loca_name'],
+                        'location_lat' =>$user_data['location_lat'],
+                        'location_long' =>$user_data['location_long'],
+                        'user_role' =>$user_data['role_flag'],
                         'is_logged_in' => true
                     );
     
                     $this->session->set_userdata($session_data);
-                    if($user_data['Role'] === "Coordinator")
-                        redirect('coordinator/dashboard');
+                    if($user_data['user_role'] === "1")
+                        redirect('Admin');
                     else
                         redirect('profile/home');
                 }
@@ -84,9 +82,9 @@ class Home extends CI_Controller
             $password = $this->input->post('password');
     
     
-            if ($this->user->check_email_exists($email)) {
-                if ($this->user->check_approved($email)) {
-                    if ($this->user->check_credentials($email, $password)) {
+            if ($this->volunteer_model->check_email_exists($email)) {
+                if ($this->volunteer_model->check_approved($email)) {
+                    if ($this->volunteer_model->check_credentials($email, $password)) {
                         return true;
                     } else {
                         $this->form_validation->set_message('login_credentials_check', 'Incorrect password');
@@ -113,12 +111,7 @@ class Home extends CI_Controller
                 //redirect("home/user_created/");
             } else {
                 $this->page_data["title"] = "Registration|" . $this->site_name;
-                //$this->load->model("helpers/captcha_helper");
-                $this->load->model("generic_model");
-                //$requestedID =  $this->generic_model->getBloodgroups();
-                $this->page_data['blood_grouplist'] = $this->generic_model->getBloodgroups();
-                $this->page_data['captcha_image'] = $this->captcha_helper->generate_captcha();
-                $this->load->view('home/registration', $this->page_data);
+                $this->load->view('users/registration', $this->page_data);
             }
         }// registration
 
@@ -133,23 +126,22 @@ class Home extends CI_Controller
 
         function register_user() {
             $this->page_data["reg_validation_errors"] = false;
-            if (isset($_POST['submit_user'])) {
-                $this->form_validation->set_rules('text_captcha', 'Captcha', 'trim|required|callback_captcha_check');
+            if (isset($_POST['btn-signup'])) {
+                //$this->form_validation->set_rules('text_captcha', 'Captcha', 'trim|required|callback_captcha_check');
                 $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|callback_register_email_check|xss_clean');
                 $this->form_validation->set_rules('firstname', 'First name', 'trim|required|xss_clean');
                 $this->form_validation->set_rules('lastname', 'Last name', 'trim|xss_clean');
-                $this->form_validation->set_rules('dobyear', 'Date of Birth', 'callback_check_dob');
+                //$this->form_validation->set_rules('dobyear', 'Date of Birth', 'callback_check_dob');
                 $this->form_validation->set_rules('gender', 'Gender', 'trim|required');
     //            $this->form_validation->set_rules('phone', 'Phone', 'trim|required|numeric|max_length[15]|xss_clean');
                 $this->form_validation->set_rules('phone', 'Phone', 'trim|required|numeric|max_length[15]|callback_register_Phone_check|xss_clean');
-                $this->form_validation->set_rules('phone_alt', 'Alternate Phone', 'trim|numeric|max_length[15]|xss_clean');
-                $this->form_validation->set_rules('bloodgroup', 'Blood Group', 'trim|required');
                 $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[8]|matches[password_re]|xss_clean');
                 $this->form_validation->set_rules('password_re', 'Confirm Password', 'trim|required|xss_clean');
                 $this->form_validation->set_rules('latitude', 'Address', 'trim|required|xss_clean|valid_lat');
+                $this->form_validation->set_rules('longitude', 'Address', 'trim|required|xss_clean|valid_long');
                 if ($this->form_validation->run()) {
                     //$key = md5(uniqid());
-                    if ($this->user->register_user($_POST['password'])) {
+                    if ($this->volunteer_model->register_user($_POST['password'])) {
                         //$this->user_registered_send_mail();
                         return true;
                     }
@@ -163,7 +155,7 @@ class Home extends CI_Controller
 
         function register_Phone_check() {
             $phone = $this->input->post('phone');
-            if ($this->user->check_phone_exists($phone)) {
+            if ($this->volunteer_model->check_phone_exists($phone)) {
                 $this->form_validation->set_message('register_Phone_check', 'Phone: ' . $phone . ' is already registered with ' . $this->site_name);
                 return false;
             }
